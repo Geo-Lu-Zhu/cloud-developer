e47d31f5299c458ae7f3f8db3a32cc54f65faa37
Geo-Lu-Zhu <88509927+Geo-Lu-Zhu@users.noreply.github.com>
2023-06-04 08:59:29 -0500

    address comments

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -27,7 +27,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
   app.get("/filteredimage", 
-      async (req, res) => {
+      async (req: express.Request, res: express.Response) => {
         const { image_url} = req.query;
         if (!image_url) {
           return res.status(400)
