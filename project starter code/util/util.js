e783d0563a2101862fd7f4b2b9cf8d1473169349
Geo-Lu-Zhu <88509927+Geo-Lu-Zhu@users.noreply.github.com>
2023-11-28 00:58:25 -0600

    updated library

--- project starter code/util/util.js
+++ project starter code/util/util.js
@@ -13,14 +13,13 @@ import Jimp from "jimp";
 export async function filterImageFromURL(inputURL) {
   return new Promise(async (resolve, reject) => {
     try {
-      const photoBuffer = await axios.get(imputURL, {
-        responseType: "arraybuffer",
+      const photoBuffer = await axios.get(inputURL, {
+        responseType: "arraybuffer"
       });
       const photo = await Jimp.read(Buffer.from(photoBuffer?.data, "binary"));
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
+      photo.resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
         .write(outpath, (img) => {
